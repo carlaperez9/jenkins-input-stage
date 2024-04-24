cbdf146763a9e50d99b902f8f351e24a2aff2989
Carla <carlaperez.irizarry9@gmail.com>
2024-04-24 14:27:47 -0400

    testing release details

--- libraries/release/steps/ReleaseDetails.groovy
+++ libraries/release/steps/ReleaseDetails.groovy
@@ -11,14 +11,15 @@ class ReleaseDetails{
 
         def myChoices = ['Choco', 'Platon', 'Luna']
         def message = "This is my message."
+        def messageRelease = "RELEASE_VERSION_GOES_HERE"
 
         // Using the input step within an input block
         def myInputs = jenkinsSteps.input(
-            id: 'userInput', // Specify an ID for the input step
+            ok: 'Confirm', // Specify an ID for the input step
             message: "${message}",
             parameters: [
                 jenkinsSteps.choice(name: "First choice", choices: myChoices, description: "This is the first choice."), 
-                jenkinsSteps.choice(name: "Second choice", choices: myChoices, description: "This is the second choice."),
+                jenkinsSteps.string(name: 'A String Goes Here', defaultValue: "", description: "We can place a description here")
             ]
         )
 
