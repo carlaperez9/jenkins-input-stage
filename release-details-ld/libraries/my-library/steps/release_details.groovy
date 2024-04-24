d0d5cc68683605c48c3e5397bab3e1c1712067ea
Carla N. Pérez <69050594+carlaperez9@users.noreply.github.com>
2024-04-24 12:29:33 -0400

    Delete release-details-ld directory

--- release-details-ld/libraries/my-library/steps/release_details.groovy
+++
@@ -1,6 +0,0 @@
-import my-library.workflow.ReleaseStage 
-
-def call(){ 
-    ReleaseStage release = new ReleaseStage(this)
-    release.run()
-}
--- release-details-ld/libraries/sonarqube/steps/static_code_analysis.groovy
+++
@@ -1,5 +0,0 @@
-void call(){ 
-  stage("Sonarqube: SCA"){
-    println "static code analysis from sonarqube library"
-  }
-}
