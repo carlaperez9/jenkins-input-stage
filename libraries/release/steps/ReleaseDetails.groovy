115be6fd169d0b02c4727ac69e5e854f7ca4491d
Carla <carlaperez.irizarry9@gmail.com>
2024-04-24 14:12:49 -0400

    testing release details

--- libraries/release/steps/ReleaseDetails.groovy
+++ libraries/release/steps/ReleaseDetails.groovy
@@ -8,6 +8,18 @@ class ReleaseDetails{
 
     void run(){ 
         jenkinsSteps.println "Running release details!"
-    }
 
+        def myChocies = ['Choco', 'Platon', 'Luna']
+        def message   = "This is my message."
+
+        def myInputs  = input(
+            ok: "Confirm", 
+            message: "${message}", 
+            parameters: [
+                choice(name: 'First choice', choices: myChocies, description: "This is a dummy choice.")
+            ]
+        )
+
+        jenkinsSteps.println "${myInputs}"
+    }
 }
