2d24e1f07000f1777af3e3b24b5c158089139ccd
Carla Perez Irizarry <carla.perezirizarry@popular.com>
2024-05-15 12:12:41 -0400

    added mock param classes

--- libraries/parameters/src/com/business/pipeline/parameters/BuildParameters.groovy
+++ libraries/parameters/src/com/business/pipeline/parameters/BuildParameters.groovy
@@ -11,6 +11,22 @@ class BuildParameters{
 
     def create(){ 
         jenkinsSteps.println "[INFO] Created parameters"
+        getParameters(jenkinsSteps)
+    }
+
+    def getParameters(jenkinsSteps){ 
+        String returnedParameters = ""
+        jenkinsSteps.println "[INFO] Libraries: ${jenkinsSteps.pipelineConfig.libraries}"
+        if (jenkinsSteps.pipelineConfig.libraries.containsKey("dummy_dev")){ 
+            returnedParameters = "dummy dev params"
+        } else if (jenkinsSteps.pipelineConfig.libraries.containsKey("dummy_prd")){ 
+            returnedParameters = "dummy prd params"
+        } else if (jenkinsSteps.pipelineConfig.libraries.containsKey("dummy_uat")){
+            returnedParameters = "dummy uat params"
+        } else { 
+            jenkinsSteps.println "no params"
+        }
+        return returnedParameters
     }
  
 }
