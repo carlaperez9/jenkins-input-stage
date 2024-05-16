0ac794f135cb65e9c0757fa2f12547f122d61c85
Carla Perez Irizarry <carla.perezirizarry@popular.com>
2024-05-16 16:28:08 -0400

    added mock param classes

--- libraries/parameters/src/com/business/pipeline/parameters/Parameters.groovy
+++ libraries/parameters/src/com/business/pipeline/parameters/Parameters.groovy
@@ -1,9 +1,10 @@
 package com.business.pipeline.parameters
 
 interface Parameters{ 
+    
     void create()
     
-    static def removeParameters(){ 
+    static void removeParameters(){ 
         jenkinsSteps.println "[inside-removeparams]"
         def job = Jenkins.instance.getItemByFullName(jenkinsSteps.env.JOB_NAME)
         job.removeProperty(ParametersDefinitionProperty.class)
