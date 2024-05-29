c3e4d477f45f88ebfd639cb710f2027c36ca9f55
Carla Perez Irizarry <carla.perezirizarry@popular.com>
2024-05-29 10:43:33 -0400

    Test

--- libraries/release/steps/ReleaseDetails.groovy
+++ libraries/release/steps/ReleaseDetails.groovy
@@ -7,40 +7,42 @@ class ReleaseDetails{
     }
 
     void run() {
-        jenkinsSteps.println "Running release details!"
-
+        jenkinsSteps.println "Running release details"
     
-        def myUnsortedChoices = ['4.1.0', '4.2.2', '4.3.1', '0.1.0', '0.1.1', '0.1.2']
-        def message = "This is my message."
-        def messageRelease = "RELEASE_VERSION_GOES_HERE"
-
-        
-        // def mySortedChoices = reverseOrder(myUnsortedChoices)
-        reverseOrder()
-        
-
         try { 
-        // Using the input step within an input block
-        def input 
-        def releaseTimeout = jenkinsSteps.timeout(time: 5, unit: 'MINUTES'){ 
-                jenkinsSteps.input(
-                ok: 'Confirm', // Specify an ID for the input step
-                message: "${messageRelease}",
-                parameters: [
-                    jenkinsSteps.choice(name: "First choice", choices: myUnsortedChoices, description: "This is the first choice."), 
-                ],  
-            )            
-        } 
-
-        jenkinsSteps.env.RELEASE_TIMEOUT = releaseTimeout
-        jenkinsSteps.println "${jenkinsSteps.env.RELEASE_TIMEOUT}"
-
+        
+        def releaseDetails = releaseDetails()
+        setEnvironmentVariables(releaseDetails)
 
     } catch (Exception e){ 
         jenkinsSteps.println "${e.getMessage()}"
     } 
  
 }
+    
+    def releaseDetails(){
+        def myUnsortedChoices = ['4.1.0', '4.2.2', '4.3.1', '0.1.0', '0.1.1', '0.1.2']
+        def message = "This is my message."
+        def messageRelease = "RELEASE_VERSION_GOES_HERE"
+
+        def releaseTimeout = jenkinsSteps.timeout(time: 5, unit: 'MINUTES'){ 
+        jenkinsSteps.input(
+        ok: 'Confirm', // Specify an ID for the input step
+        message: "${messageRelease}",
+        parameters: [
+            jenkinsSteps.choice(name: "First choice", choices: myUnsortedChoices, description: "This is the first choice."), 
+        ],  
+    )    
+    return releaseTimeout        
+} 
+    }
+    def setEnvironmentVariables(releaseDetails){ 
+        jenkinsSteps.env.RELEASE_DETAILS        = releaseTimeout
+        def (applicationName, version)          = releaseDetails.split('-')
+        jenkinsSteps.env.applicationName        = applicationName 
+        jenkinsSteps.env.applicationVersion     = version 
+    }
+
  def reverseOrder(){ 
         def versions = ['1.1.0', '1.0.1', '4.0.0', '4.1.2']
         def mySortedList = []
