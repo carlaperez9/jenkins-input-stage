bb34f46893a29bbd85b213bd5481ab64bb9e3435
Carla Perez Irizarry <carla.perezirizarry@popular.com>
2024-05-29 10:14:42 -0400

    Test

--- libraries/release/steps/ReleaseDetails.groovy
+++ libraries/release/steps/ReleaseDetails.groovy
@@ -28,7 +28,6 @@ class ReleaseDetails{
                 message: "${messageRelease}",
                 parameters: [
                     jenkinsSteps.choice(name: "First choice", choices: myUnsortedChoices, description: "This is the first choice."), 
-                    jenkinsSteps.choice(name: "Second choice", choices: myUnsortedChoices, description: "This is the second choice."), 
                 ],  
             )            
         } 
